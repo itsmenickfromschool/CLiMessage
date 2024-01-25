290c8c3a05898917e06c94ba8e443f5a20916620
Nick Burr <nicholasstclairburr@gmail.com>
2024-01-25 15:24:14 -0700

    terminal prompt for sending a text.

--- Main.py
+++ Main.py
@@ -3,18 +3,18 @@ import time
 
 x = pyautogui.position()[0]
 y = pyautogui.position()[1]
-print(f"({x} ,{y})")
+# print(f"({x} ,{y})")
 
-print(pyautogui.position())
-print(pyautogui.PAUSE)
-print(pyautogui.FAILSAFE)
+# print(pyautogui.position())
+# print(pyautogui.PAUSE)
+# print(pyautogui.FAILSAFE)
 
 person = input("Enter the contact you want to text: ")
 message = input(f"\nEnter your message to {person}: ")
 
 def confirmSend():
     while True:
-        res = input(f"Are you sure you want to send {person} the following message: \n {message}\n\n Type yes/no to continue...")
+        res = input(f"Are you sure you want to send {person} the following message: \n\n {message}\n\n (Y/N?): ")
         if res in ['yes','y','true','ye','t']:
             return True
         elif res in ['no','n','false','quit','f']:
@@ -22,6 +22,11 @@ def confirmSend():
         else:
             print("Please enter a valid response (yes/no)")
 
-confirmSend()
-# def sendTextMessage(person, message):
+def sendTextMessage(person, message):
+    
+
+# confirmSend()
+if confirmSend():
+    sendTextMessage(person, message)
+
     
